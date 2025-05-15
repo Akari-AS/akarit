841076cdd5c6943a410e2edbee483bcd1ab4128f
DiggsAsura <kenneth.bjerke@gmail.com>
2025-05-15 16:52:15 +0200

    skjema og footer styling

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,54 @@
 </main> <?php // Main tag lukkes her ?>
 
-<footer>
-    <div class="container">
-        <p>© <?php echo date("Y"); ?> Akari AS. Alle Rettigheter Reservert. | Sertifisert Google Workspace Partner</p> <!-- Endret fra Akarit (av Akari AS) -->
+<footer class="site-footer">
+    <div class="container footer-container">
+        <div class="footer-column footer-column-nav">
+            <h4>Om Akari</h4>
+            <ul>
+                <li><a href="https://akari.no/vilkar-og-betingelser" target="_blank">Vilkår og betingelser</a></li>
+                <li><a href="https://akari.no/personvern" target="_blank">Personvern og cookies</a></li>
+            </ul>
+            <h4>Sosiale Medier</h4>
+            <ul>
+                <li><a href="https://www.facebook.com/akarias" target="_blank">Facebook</a></li>
+                <li><a href="https://www.instagram.com/akari_as/" target="_blank">Instagram</a></li>
+                <li><a href="https://www.linkedin.com/company/akari-as/" target="_blank">LinkedIn</a></li>
+                <li><a href="https://www.youtube.com/channel/UCoy3JgIe3p50Pq3f52zftFA" target="_blank">YouTube</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-column footer-column-contact">
+            <h4>Kontakt oss</h4>
+            <p>
+                e: <a href="mailto:kreative@akari.no">kreative@akari.no</a><br>
+                t: <a href="tel:+4732766600">32 76 66 00</a>
+            </p>
+            <h4>Åpningstider:</h4>
+            <p>Hverdager 08.00 – 16.00</p>
+            
+            <h4>Websupport</h4>
+            <p>
+                e: <a href="mailto:support@akari.no">support@akari.no</a><br>
+                t: <a href="tel:+4732766600">32 76 66 00</a>
+            </p>
+        </div>
+
+        <div class="footer-column footer-column-brand">
+            <img src="/assets/img/Akari_jubileum.svg" alt="Akari Logo" class="footer-logo">
+            <div class="footer-locations">
+                Kongsberg • Hønefoss • Notodden • Numedal
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <p>© <?php echo date("Y"); ?> Akari AS</p>
+        </div>
     </div>
 </footer>
 
 <!-- Link til VÅR JavaScript fil -->
-<script src="/assets/js/script.js"></script> <?php // Defer er ikke nødvendig hvis scriptet er nederst ?>
+<script src="/assets/js/script.js"></script>
 
-</body> <?php // Body tag lukkes her ?>
+</body>
 </html>
