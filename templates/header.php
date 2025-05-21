23627ce5e79f77d45bf921144ea8b568f23ab0a2
DiggsAsura <kenneth.bjerke@gmail.com>
2025-05-21 11:18:26 +0200

    ny header

--- templates/header.php
+++ templates/header.php
@@ -11,16 +11,12 @@
     <!-- Favicon -->
     <link rel="icon" href="/assets/img/favicon_akari.png" type="image/png">
 
-    <!-- Start CookieYes Banner - Plassert tidlig for samtykkehåndtering -->
-    <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/edac3f0861b009c0f5dbf4b9/script.js"></script>
-    <!-- End CookieYes Banner -->
-
-    <!-- Google Tag Manager - Lastes etter CookieYes for å potensielt styres av samtykke -->
+    <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-5B2PFWHX');</script> <!-- DIN NYE GTM ID -->
+    })(window,document,'script','dataLayer','GTM-5B2PFWHX');</script> <!-- DIN GTM ID -->
     <!-- End Google Tag Manager -->
 
     <!-- Google Fonts (Red Hat Display) -->
@@ -93,7 +89,7 @@
 <body class="bg-akari-light-green text-gray-800 font-body antialiased">
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5B2PFWHX"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> <!-- DIN NYE GTM ID -->
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> <!-- DIN GTM ID -->
     <!-- End Google Tag Manager (noscript) -->
 
 <header>
