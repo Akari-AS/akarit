24a0e57f3acd1dd6ff04def0fc14583579b48dfd
DiggsAsura <kenneth.bjerke@gmail.com>
2025-09-10 16:22:39 +0200

    dynamisk versjon

---
+++ public/internal/salgsprognose/config.js
@@ -0,0 +1,13 @@
+// IMPORTANT: Do not share this file or expose your API key.
+// It's recommended to restrict the API key in your Google Cloud Console.
+
+const config = {
+    apiKey: 'AIzaSyBMMsQCGJrvGNUMkTUfrrt6gW-u4Uf6u-8',
+    sheetId: '1BsZK2eqNQg7SuT17d4z-CFmzbjdKwZcs2renQCPbVXQ',
+    sheets: {
+        sales: 'Salg',
+        pipeline: 'Pipeline',
+        resources: 'Ressurser',
+        budget: 'Budsjett'
+    }
+};
